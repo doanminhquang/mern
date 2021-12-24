74029f1ad1b9e962c4538c467e382ebc3c6b6ad3
Đoàn Minh Quang <42968935+doanminhquang@users.noreply.github.com>
2021-12-24 17:49:56 +0700

    Update sendsms.js

--- server/code/sendsms.js
+++ server/code/sendsms.js
@@ -1,70 +1,57 @@
-const SerialPort = require("serialport");
+var SerialPort = require("serialport");
 
-const Build = () => {
-  SerialPort.list().then((ports) => {
-    ports.forEach(function (port) {
-      var pnpId = "USB\\VID_0408&PID_EA26&MI_01";
-      if (port.pnpId.toString().includes(pnpId)) {
-        return port.path;
-      }
-    });
-  });
-};
+var myport = "";
 
-class PhoneSMS {
-  constructor() {
-    this.Myport = new SerialPort("COM11", {
-      baudrRate: 9600,
-      dataBits: 8,
-      parity: "none",
-      stopBits: 1,
-      flowControl: false,
-    });
-  }
-
-  getList = () => {
-    SerialPort.list().then((ports) => {
-      ports.forEach(function (port) {
-        console.log(port);
+SerialPort.list().then((ports) => {
+  ports.forEach(function (port) {
+    var pnpId = "USB\\VID_0408&PID_EA26&MI_01";
+    if (port.pnpId.toString().includes(pnpId)) {
+      myport = new SerialPort(`${port.path}`, {
+        baudrRate: 9600,
+        dataBits: 8,
+        parity: "none",
+        stopBits: 1,
+        flowControl: false,
       });
-    });
-  };
-
-  onOpen = (error) => {
-    if (!error) {
-      console.log("Kết nối thành công");
+      myport.on("open", onOpen);
+      myport.on("error", onError);
+      myport.on("data", onDataReceived);
+      myport.on("close", onClose);
     }
-  };
+  });
+});
 
-  onDataReceived = (data) => {
-    console.log("Phản hồi: " + data);
-  };
-
-  onError = (error) => {
-    console.log(error);
-  };
-
-  onClose = (error) => {
-    console.log("Ngắt kết nối");
-    console.log(error);
-  };
-
-  sendcode = (toPhone, code, time) => {
-    setTimeout(() => {
-      this.Myport.write("AT+CMGF=1\r");
-      setTimeout(() => {
-        this.Myport.write(`AT+CMGS="${toPhone}"\r`);
-        setTimeout(() => {
-          this.Myport.write(
-            `Ma khoi phuc: ${code}\rHet hieu luc sau 10 phut: ${time}.`
-          );
-          setTimeout(() => {
-            this.Myport.write("\x1A");
-          }, 100);
-        }, 100);
-      }, 100);
-    }, 1000);
-  };
+function onOpen(error) {
+  if (!error) {
+    console.log("Kết nối thành công");
+    send(myport, "0916225856", "Hi");
+  }
 }
 
-module.exports = PhoneSMS;
+function onDataReceived(data) {
+  console.log("Phản hồi: " + data);
+}
+
+function onError(error) {
+  console.log(error);
+}
+
+function onClose(error) {
+  console.log("Ngắt kết nối");
+  console.log(error);
+}
+
+function send(serial, toPhone, message) {
+  setTimeout(() => {
+    serial.write("AT+CMGF=1\r");
+    setTimeout(() => {
+      serial.write(`AT+CMGS="${toPhone}"\r`);
+      setTimeout(() => {
+        serial.write(`${message}\r`);
+        setTimeout(() => {
+          serial.write("\x1A");
+        }, 100);
+      }, 100);
+    }, 100);
+  }, 100);
+}
